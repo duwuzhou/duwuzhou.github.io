d6b876f3a432b119bf8cbaf30347e5a6714f7d02
duwuzhou <1753242279@qq.com>
2025-04-05 21:50:35 +0800

    问题

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 export default defineConfig({
   // 如果你的仓库名是 username.github.io，使用 './'
   // 如果是其他仓库名，使用 '/your-repo-name/'
-  base: '/duwuzhou.github.io',
+  base: '/duwuzhou.github.io/',
   plugins: [vue()],
   resolve: {
     alias: {
